03a683938abc074b6b47b127c118b174726c7882
Chris Martin <ch.martin@gmail.com>
2016-04-11 00:46:12 -0700

    update nix

--- aligned.nix
+++ aligned.nix
@@ -1,8 +1,8 @@
-{ mkDerivation, base, hscolour, stdenv }:
+{ mkDerivation, base, stdenv }:
 mkDerivation {
   pname = "aligned";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base hscolour ];
+  libraryHaskellDepends = [ base ];
   license = stdenv.lib.licenses.asl20;
 }
